aabb8e03d73d1e46461a24b7a1f57b3e4d5b503f
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-07-24 19:13:56 -0500

    added parties table

--- server.js
+++ server.js
@@ -91,16 +91,6 @@ app.post('/api/candidate', ({ body }, res) => {
         });
     });
 });
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                         VALUES (?,?,?,?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
-
-// db.query(sql, params, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
 
 app.get('/', (req, res) => {
     res.json({
